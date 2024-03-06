c28cd0e9536de7f57a761b36c0bffa6ccc3e2ad0
Aaron3312 <a01642529@tec.mx>
2024-03-05 19:59:59 -0600

    inicio

---
+++ Inicio.py
@@ -0,0 +1,19 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'findNumber' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY arr
+#  2. INTEGER k
+#
+
+def findNumber(arr, k):
+    # Write your code here
